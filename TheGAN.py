e0a7dd644a0a1f772d05732bf237d8b8738a54b4
andyElking <andraz.jelincic@gmail.com>
2022-09-29 11:59:07 +0100

    improved model saving and loading

--- TheGAN.py
+++ TheGAN.py
@@ -15,7 +15,6 @@ from aux_functions import *
 from Generator import Generator
 from Discriminator import Discriminator
 
-
 config = {
     'device': torch.device('cpu'),
     'noise size': 62,
@@ -43,19 +42,52 @@ config = {
     'W fixed whole': [1.0, -0.5, -1.2, -0.3, 0.7, 0.2, -0.9, 0.1, 1.7]
 }
 
-init_config(config)
+training_config = {
+    'num epochs': 20,
+    'num Chen iters': 5000,
+    'optimizer': 'Adam',
+    'lrG': 0.0001,
+    'lrD': 0.0005,
+    'beta1': 0,
+    'beta2': 0.99,
+    'weight clipping limit': 0.01,
+    'gp weight': 10.0,
+    'batch size': 1024,
+}
 
 
 class LevyGAN:
 
     def __init__(self, cf: dict):
-        self.device = cf['device']
+        init_config(cf)
 
+        # ============ Model config ===============
+        self.device = cf['device']
+        self.ngpu = cf['ngpu']
         self.noise_size = cf['noise size']
         self.w_dim = cf['w dim']
         self.a_dim = cf['a dim']
         self.s_dim = cf['s dim']
 
+        # if 1 use GAN1, if 2 use GAN2, etc.
+        self.which_discriminator = cf['which discriminator']
+        self.which_generator = cf['which generator']
+
+        self.generator_symmetry_mode = cf['generator symmetry mode']
+        self.generator_last_width = cf['generator last width']
+
+        # slope for LeakyReLU
+        self.leakyReLU_slope = cf['leakyReLU slope']
+
+        self.T, self.M, self.S = generate_TMS(self.w_dim)
+
+        # create the nets
+        self.netG = Generator(cf)
+        self.netD = Discriminator(cf)
+
+        self.dict_saves_filename = f'_G{self.which_generator}_D{self.which_discriminator}_{self.generator_symmetry_mode}_{self.w_dim}d_{self.noise_size}noise'
+
+        # ============== Training config ===============
         # Number of training epochs using classical training
         self.num_epochs = cf['num epochs']
 
@@ -69,12 +101,19 @@ class LevyGAN:
         self.lrG = cf['lrG']
         self.lrD = cf['lrD']
 
-        # Beta1 hyperparam for Adam optimizers
+        # Beta hyperparam for Adam optimizers
         self.beta1 = cf['beta1']
-
         self.beta2 = cf['beta2']
 
-        self.ngpu = cf['ngpu']
+        if self.which_optimizer == 'Adam':
+            self.optG = torch.optim.Adam(self.netG.parameters(), lr=self.lrG, betas=(self.beta1, self.beta2))
+            self.optD = torch.optim.Adam(self.netD.parameters(), lr=self.lrD, betas=(self.beta1, self.beta2))
+        elif self.which_optimizer == 'RMSProp':
+            self.optG = torch.optim.RMSprop(self.netG.parameters(), lr=self.lrG)
+            self.optD = torch.optim.RMSprop(self.netD.parameters(), lr=self.lrD)
+
+        # this gives the option to rum the training process multiple times with differently initialised GANs
+        self.num_trials = cf['num trials']
 
         # To keep the criterion Lipschitz
         self.weight_cliping_limit = cf['weight clipping limit']
@@ -84,36 +123,12 @@ class LevyGAN:
 
         self.batch_size = cf['batch size']
 
-        self.test_batch_size = cf['test batch size']
-
-        # if 1 use GAN1, if 2 use GAN2, etc.
-        self.which_discriminator = cf['which discriminator']
-        self.which_generator = cf['which generator']
-        self.generator_symmetry_mode = cf['generator symmetry mode']
-        self.generator_last_width = cf['generator last width']
-
-        # slope for LeakyReLU
-        self.leakyReLU_slope = cf['leakyReLU slope']
-
-        # this gives the option to rum the training process multiple times with differently initialised GANs
-        self.num_trials = cf['num trials']
-
+        # ============ Testing config ============
         self.num_tests_for2d = cf['num tests for 2d']
 
+        self.test_batch_size = cf['test batch size']
+
         self.W_fixed_whole = cf['W fixed whole']
-
-        self.T, self.M, self.S = generate_TMS(self.w_dim)
-
-        self.netG = Generator(cf)
-        self.netD = Discriminator(cf)
-
-        if self.which_optimizer == 'Adam':
-            self.optG = torch.optim.Adam(self.netG.parameters(), lr=self.lrG, betas=(self.beta1, self.beta2))
-            self.optD = torch.optim.Adam(self.netD.parameters(), lr=self.lrD, betas=(self.beta1, self.beta2))
-        elif self.which_optimizer == 'RMSProp':
-            self.optG = torch.optim.RMSprop(self.netG.parameters(), lr=self.lrG)
-            self.optD = torch.optim.RMSprop(self.netD.parameters(), lr=self.lrD)
-
         self.W_fixed = torch.tensor(self.W_fixed_whole)[:self.w_dim].unsqueeze(1).transpose(1, 0)
         self.W_fixed = self.W_fixed.expand((self.test_batch_size, self.w_dim))
 
@@ -125,11 +140,14 @@ class LevyGAN:
                             self.w_dim:(self.w_dim + self.a_dim)]
 
         unfixed_test_data_filename = f"samples/non-fixed_samples_{self.w_dim}-dim.csv"
-        self.unfixed_test_data = np.genfromtxt(unfixed_test_data_filename, dtype = float, delimiter=',')[:self.test_batch_size]
+        self.unfixed_test_data = np.genfromtxt(unfixed_test_data_filename, dtype=float, delimiter=',')[
+                                 :self.test_batch_size]
 
         self.fixed_data_for_2d = []
         if self.w_dim == 2:
-            self.fixed_data_for_2d = [np.genfromtxt(f"samples/fixed_samples_2-dim{i+1}.csv",dtype=float,delimiter=',') for i in range(self.num_tests_for2d)]
+            self.fixed_data_for_2d = [
+                np.genfromtxt(f"samples/fixed_samples_2-dim{i + 1}.csv", dtype=float, delimiter=',') for i in
+                range(self.num_tests_for2d)]
 
     def _gradient_penalty(self, real_data, generated_data):
         b_size_gp = real_data.size()[0]
@@ -147,7 +165,8 @@ class LevyGAN:
 
         # Calculate gradients of probabilities with respect to examples
         gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
-                               grad_outputs=torch.ones(prob_interpolated.size()).cuda() if self.ngpu > 0 else torch.ones(
+                               grad_outputs=torch.ones(
+                                   prob_interpolated.size()).cuda() if self.ngpu > 0 else torch.ones(
                                    prob_interpolated.size(), device=self.device),
                                create_graph=True, retain_graph=True)[0]
 
@@ -195,10 +214,14 @@ class LevyGAN:
         difference = np.abs(self.st_dev_W_fixed - np.sqrt(np.abs(empirical_second_moments(_a_generated))))
         return difference.mean()
 
-    def make_report(self, epoch: int = None, iter: int = None):
+    def make_report(self, epoch: int = None, iters: int = None, add_line_break=True):
         report = ""
-        if not (epoch is None or iter is None):
-            report = f"epoch: {epoch}/{self.num_epochs}, iter: {iter}, "
+        if add_line_break:
+            line_break = "\n"
+        else:
+            line_break = " "
+        if not (epoch is None or iters is None):
+            report = f"epoch: {epoch}/{self.num_epochs}, iter: {iters}, "
 
         pretty_chen_errors = make_pretty(self.chen_errors())
 
@@ -225,27 +248,50 @@ class LevyGAN:
         if self.w_dim > 2:
             noise = torch.randn((self.test_batch_size, self.noise_size), dtype=torch.float, device=self.device)
             g_in = torch.cat((noise, self.W_fixed), 1)
-            A_fixed_gen = self.netG(g_in)[:, self.w_dim:self.w_dim + self.a_dim].detach().numpy()
-            errors = [sqrt(ot.wasserstein_1d(self.A_fixed_true[:, i], A_fixed_gen[:, i], p=2)) for i in range(self.a_dim)]
+            a_fixed_gen = self.netG(g_in)[:, self.w_dim:self.w_dim + self.a_dim].detach().numpy()
+            errors = [sqrt(ot.wasserstein_1d(self.A_fixed_true[:, i], a_fixed_gen[:, i], p=2)) for i in
+                      range(self.a_dim)]
             pretty_errors = make_pretty(errors)
-            st_dev_err = self.avg_st_dev_error(A_fixed_gen)
-            joint_err = joint_wass_dist(self.A_fixed_true[:1000],A_fixed_gen[:1000])
+            st_dev_err = self.avg_st_dev_error(a_fixed_gen)
+            joint_err = joint_wass_dist(self.A_fixed_true[:1000], a_fixed_gen[:1000])
             pretty_chen_errors = make_pretty(self.chen_errors())
-            report += f", st_dev error: {st_dev_err: .4f}, joint_wass_dist: {joint_err: .5f}\nerrs: {pretty_errors}, ch_err: {pretty_chen_errors} "
+            report += f", st_dev error: {st_dev_err: .4f}, joint_wass_dist: {joint_err: .5f}{line_break}errs: {pretty_errors}, ch_err: {pretty_chen_errors} "
         else:
             pretty_errors = make_pretty(self.all_2dim_errors())
-            report += f"\nerrs: {pretty_errors}, ch_err: {pretty_chen_errors[0]}"
+            report += f"{line_break}errs: {pretty_errors}, ch_err: {pretty_chen_errors[0]}"
         return report
 
     def load_dicts(self, descriptor: str = ""):
-        self.netG.load_state_dict(torch.load(f'model_saves/generator{self.which_generator}_{self.generator_symmetry_mode}_{self.w_dim}d_{self.noise_size}noise_{descriptor}.pt'))
-        self.netD.load_state_dict(torch.load(f'model_saves/discriminator{self.which_discriminator}_{self.generator_symmetry_mode}_{self.w_dim}d_{self.noise_size}noise_{descriptor}.pt'))
+        self.netG.load_state_dict(torch.load(f'model_saves/generator' + self.dict_saves_filename + f'_{descriptor}.pt'))
+        self.netD.load_state_dict(torch.load(f'model_saves/discriminator' + self.dict_saves_filename + f'_{descriptor}.pt'))
 
-    def save_dicts(self, descriptor: str = ""):
-        paramsD = copy.deepcopy(self.netD.state_dict())
-        paramsG = copy.deepcopy(self.netG.state_dict())
-        torch.save(paramsD, f'model_saves/discriminator{self.which_discriminator}_{self.generator_symmetry_mode}_{self.w_dim}d_{self.noise_size}noise_{descriptor}.pt')
-        torch.save(paramsG, f'model_saves/generator{self.which_generator}_{self.generator_symmetry_mode}_{self.w_dim}d_{self.noise_size}noise_{descriptor}.pt')
+    def load_dicts_unstructured(self, gen_filename, discr_filename):
+        self.netG.load_state_dict(torch.load(gen_filename))
+        self.netD.load_state_dict(torch.load(discr_filename))
+
+    def save_current_dicts(self, descriptor: str = ""):
+        params_g = copy.deepcopy(self.netG.state_dict())
+        params_d = copy.deepcopy(self.netD.state_dict())
+        self.save_dicts(params_g, params_d, descriptor)
+
+    def save_dicts(self, params_g, params_d, descriptor: str = ""):
+        filename = f'model_saves/summary_of_models' + self.dict_saves_filename + '.txt'
+
+        with open(filename, 'a+') as summary_file:
+            summary_file.seek(0)
+            lines = summary_file.read().splitlines()
+            if not lines:
+                serial_num = 1
+                summary_file.write(str(serial_num))
+            else:
+                last_line = lines[-1]
+                serial_num = int(last_line)
+
+            summary = f" {descriptor}: " + self.make_report(add_line_break=False) + f"\n{serial_num + 1}"
+            summary_file.write(summary)
+
+        torch.save(params_g, f'model_saves/generator' + self.dict_saves_filename + f'_{serial_num}_{descriptor}.pt')
+        torch.save(params_d, f'model_saves/discriminator' + self.dict_saves_filename + f'_{serial_num}_{descriptor}.pt')
 
     def compute_wth(self, w_in: torch.Tensor, h_in: torch.Tensor):
         return aux_compute_wth(w_in, h_in, self.S, self.T, self.w_dim)
