77940467d1698c9e8dadf22bace3227da63eb2c3
andyElking <andraz.jelincic@gmail.com>
2022-10-04 11:02:51 +0100

    fixed chech_learning_rates

--- varying_hyperparams.py
+++ varying_hyperparams.py
@@ -58,7 +58,8 @@ def check_learning_rates():
     gan.do_tests()
     report = gan.make_report(add_line_break=False)
     gan.save_current_dicts(report=report, descriptor="INITIAL")
-    for mult in [20, 10, 5, 2, 1, 0.5]:
+    mults = [20, 10, 5, 2, 1, 0.5]
+    for mult in mults:
         result_row = []
         for lr_g in lr_gs:
             lr_d = mult * lr_g
@@ -71,19 +72,23 @@ def check_learning_rates():
             min_chen_sum = gan.test_results['min chen sum']
             gan.load_dicts(descriptor=f'lrG_{lr_g:.6f}_lrD_{lr_d:.6f}_min_sum')
             gan.do_tests(comp_joint_err=True)
-            joint_err_min_sum = gan.test_results['joint_wass_error']
+            joint_err_min_sum = gan.test_results['joint wass error']
             gan.load_dicts(descriptor=f'lrG_{lr_g:.6f}_lrD_{lr_d:.6f}min_chen')
             gan.do_tests(comp_joint_err=True)
-            joint_err_min_chen = gan.test_results['joint_wass_error']
+            joint_err_min_chen = gan.test_results['joint wass error']
             entry = f" {min_sum:.5f}, {min_chen_sum:.5f}, {joint_err_min_sum:.5f}, {joint_err_min_chen:.5f} "
             result_row.append(entry)
         result_grid.append(result_row)
 
-    nice_results = pandas.DataFrame(result_grid, columns=[0.0001, 0.00005, 0.00002, 0.00001, 0.000005, 0.000002], index=[20, 10, 5, 2, 1, 0.5])
-    filename = f"model_saves/{gan.dict_saves_folder}/LRs_result_grid_{gan.dict_saves_folder}.txt"
+    nice_results = pandas.DataFrame(result_grid, columns=lr_gs, index=mults)
+    filename = f"model_saves/{gan.dict_saves_folder}/LRs_result_grid_num{gan.serial_number}_{gan.dict_saves_folder}.txt"
     with open(filename, 'a+') as file:
         file.write("\n")
         file.write(nice_results.to_string())
+
+
+def check_gp_and_leaky():
+    print("a")
 
 
 def check_betas():
@@ -152,7 +157,7 @@ def check_betas():
         result_grid.append(result_row)
 
     nice_results = pandas.DataFrame(result_grid, columns=[0.0, 0.05, 0.1, 0.5, 0.8, 0.95], index=[0.95, 0.98, 0.999])
-    filename = f"model_saves/{gan.dict_saves_folder}/betas_result_grid_{gan.dict_saves_folder}.txt"
+    filename = f"model_saves/{gan.dict_saves_folder}/LRs_result_grid_num{gan.serial_number}_{gan.dict_saves_folder}.txt"
     with open(filename, 'a+') as file:
         file.write("\n")
         file.write(nice_results.to_string())
